3d037f52343876fef49cfbfdc6d888523eeba2e0
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-06-23 17:27:33 +0200

    Exercice

--- JavaScript/ex-jeu.js
+++ JavaScript/ex-jeu.js
@@ -1,27 +1,67 @@
+function getRandom() {
+  return Math.random();
+}
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+}
+
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+}
+
 // import d'APIs de Node.js
 // (dans le navigateur process et readline n'existent pas)
-const { stdin, stdout } = require('process');
-const readline = require('readline');
+const { stdin, stdout } = require("process");
+const readline = require("readline");
 
 // configure readline (qui lit une ligne)
 // pour qu'il lise la ligne sur le clavier (stdin) et afficher
 // les questions dans le terminal (stdout)
 const rl = readline.createInterface({
   input: stdin,
-  output: stdout
+  output: stdout,
 });
+const entierAlea = getRandomInt(0, 100);
+const essais = [];
 
 function jouer() {
+  if (essais.length) {
+    console.log(`Vous avez déjà saisi : ${essais.join(' - ')}`);
+  }
+
   // pose une question, le callback est asynchrone
-  rl.question('Quel est le nombre ? ', (answer) => {
+  rl.question("Quel est le nombre ? ", (answer) => {
     // answer est de type string, TODO le convertir en number
-    console.log('Vous avez saisi : ' + answer);
+    console.log("Vous avez saisi : " + answer);
 
-    // repose la même la question
-    jouer();
+    const entierSaisi = Number.parseInt(answer, 10);
 
-    // arrête la partie
-    // rl.close();
+    if (Number.isNaN(entierSaisi)) {
+      console.log('Erreur : il faut saisir un nombre');
+      return jouer();
+    }
+
+    essais.push(entierSaisi);
+
+    if (entierSaisi > entierAlea) {
+      console.log("Trop grand");
+      jouer();
+    } else if (entierSaisi < entierAlea) {
+      console.log("Trop petit");
+      jouer();
+    } else {
+      console.log("Gagné");
+      rl.close();
+    }
   });
 }
 
