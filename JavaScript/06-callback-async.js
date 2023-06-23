d2ff782831105bd9051a227428c5b9f2624a0795
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-06-23 15:07:01 +0200

    Event Loop

--- JavaScript/06-callback-async.js
+++ JavaScript/06-callback-async.js
@@ -9,6 +9,7 @@ console.log('E');
 // Réponse 1 - A B C D E
 // Réponse 2 - E B A D C
 // Réponse 3 - B E A D C
+// Réponse 4 - E B D A C
 
 
 // du code asynchrone s'exécute plus tard
@@ -36,3 +37,14 @@ console.log('E');
 // File d'attente (502ms) :
 // File d'attente (1000ms) : taskC
 // File d'attente (1001ms) :
+
+// Attention en pratique si j'appelle plusieurs fonction async
+// en même temps, je ne sais pas dans quel ordre les callbacks vont s'exécuter
+// (setTimeout un cas particulier)
+// fs.readFile('file1.txt', (err, data) => console.log('file1.txt read'));
+// fs.readFile('file2.txt', (err, data) => console.log('file2.txt read'));
+// fetch('url1').then((res) => console.log('réponse 1'));
+// fetch('url2').then((res) => console.log('réponse 2'));
+// btn1.addEventListener('click', () => console.log('click btn1'));
+// btn2.addEventListener('click', () => console.log('click btn2'));
+// console.log('FIN');
