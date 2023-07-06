504a2660bbedd3c8eadaf446906462f75efcdb45
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-07-06 10:30:32 +0200

    Correction Web APIs

--- Web-APIs/ex-todos/main.js
+++ Web-APIs/ex-todos/main.js
@@ -17,6 +17,13 @@ const toggleEl = document.querySelector(".todos-global-checkbox");
 
 formEl.addEventListener("submit", (event) => {
   event.preventDefault();
+  inputEl.classList.remove('error');
+
+  if (inputEl.value === '') {
+    inputEl.classList.add('error');
+    return;
+  }
+
   const todoRowEl = createTodoRow({
     id: Math.random(),
     title: inputEl.value,
@@ -36,18 +43,21 @@ toggleEl.addEventListener("click", () => {
 
 listEl.addEventListener("click", (event) => {
   if (event.target.classList.contains("todos-btn-delete")) {
-    event.target.closest("todos-row").remove();
+    event.target.closest(".todos-row").remove();
   }
 });
 
-listEl.addEventListener('dblclick', (event) => {
+listEl.addEventListener("dblclick", (event) => {
   if (event.target.classList.contains("todos-value")) {
     event.target.replaceWith(createInput(event.target.innerText));
   }
 });
 
-listEl.addEventListener('keyup', (event) => {
-  if (event.target.classList.contains("todos-value-edit") && event.code === 'Enter') {
+listEl.addEventListener("keyup", (event) => {
+  if (
+    event.target.classList.contains("todos-value-edit") &&
+    event.code === "Enter"
+  ) {
     event.target.replaceWith(createSpan(event.target.value));
   }
 });
@@ -57,14 +67,32 @@ listEl.addEventListener('keyup', (event) => {
 // https://jsonplaceholder.typicode.com/todos/
 // Boucler sur le tableau reçu en JSON et afficher
 // les données en appelant createTodoRow
+fetch("https://jsonplaceholder.typicode.com/todos", {
+  method: "GET",
+})
+  .then((res) => res.json())
+  .then((todos) => {
+    for (const todo of todos) {
+      const todoRowEl = createTodoRow(todo);
+      listEl.append(todoRowEl);
+    }
+  });
 
 // Exercice 7
 // Au submit du formulaire afficher la bordure
 // du champs en rouge si aucune valeur n'a été saisie
 // et ne pas ajouter la ligne
 
+
 // Exercice 8
 // Ecouter l'événément input sur le champ
 // Stocker la saisie dans le localStorage
 // Au chargement de la page reremplir le champ avec
 // le contenu du localStorage
+inputEl.addEventListener('input', () => {
+  localStorage.setItem('todo-value', inputEl.value);
+});
+
+if (localStorage.getItem('todo-value')) {
+  inputEl.value = localStorage.getItem('todo-value');
+}
